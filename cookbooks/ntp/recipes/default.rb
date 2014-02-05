94676ed491a3fe6d985ba2ba69cbca5f8ee00116
Freidah, Alexander <alex.freidah@patch.com>
2014-02-05 14:59:27 -0500

    added bare-bones cookbook for ntp to do some vagrant testing

---
+++ cookbooks/ntp/recipes/default.rb
@@ -0,0 +1,19 @@
+#
+# Cookbook Name:: ntp
+# Recipe:: default
+#
+# Copyright 2014, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+package 'ntp'
+
+template '/etc/ntp.conf' do
+  source    'ntp.conf.erb'
+  notifies  :restart, 'service[ntpd]'
+end
+
+service 'ntpd' do
+  action [:enable, :start]
+end
