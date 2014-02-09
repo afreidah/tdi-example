b09ff687184538393353aca567c8f8425763369e
Alex Freidah <alexfreidah@Alexs-MacBook.local>
2014-02-08 21:37:37 -0500

    added cookbooks for building ruby

---
+++ roles/developer.rb
@@ -0,0 +1,14 @@
+name "developer"
+description "for development machines"
+run_list "recipe[git]", "recipe[chruby::system]"
+
+default_attributes(
+  "chruby" => {
+    "rubies" => {
+      "1.9.3-p392" => false,
+      "1.9.3-p484" => true,
+      "2.0.0-p353" => true
+    },
+    "default" => "2.0.0-p353"
+  }
+)
